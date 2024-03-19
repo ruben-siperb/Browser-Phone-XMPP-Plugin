95c899d1cff6ca77eae4dfbc759c2256879c05ff
rubenvanbreda-work <163388393+ruben-siperb@users.noreply.github.com>
2024-03-19 10:05:37 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,15 +1,12 @@
 exports.helloworld = function (){
-    console.log("Hello World!");
     return "Hello World!";
 }
 
 exports.connect = function () {
-    console.log("Connecting...");
     return "Connecting...";
 };
 
 exports.disconnect = function () {
-    console.log("Disconnecting...");
     return "Disconnecting...";
 };
 
