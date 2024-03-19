485c751573922a8b23319e8cb9a7286fc378da8e
rubenvanbreda-work <163388393+ruben-siperb@users.noreply.github.com>
2024-03-19 09:52:07 +0200

    added test plugin
    added plugin to check node modules install

---
+++ simple_plugin/plugin.js
@@ -0,0 +1,14 @@
+exports.helloworld = function (){
+    console.log("Hello World!");
+    return "Hello World!";
+}
+
+exports.connect = function () {
+    console.log("Connecting...");
+    return "Connecting...";
+};
+
+exports.disconnect = function () {
+    console.log("Disconnecting...");
+    return "Disconnecting...";
+};
